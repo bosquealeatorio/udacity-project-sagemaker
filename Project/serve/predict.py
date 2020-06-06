3f533a0ec9f12045a21fffa824ef8f2d4e7342e4
EC2 Default User <ec2-user@ip-172-16-64-97.us-east-2.compute.internal>
2020-06-06 05:12:23 +0000

    [update] proyect completed

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,9 +69,12 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
+    
+    input_words = review_to_words(input_data)
+    input_words, input_length = convert_and_pad(model.word_dict, input_words)
 
-    data_X = None
-    data_len = None
+    data_X = input_words
+    data_len = input_length
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -86,7 +89,7 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    output = model(data).detach().cpu().numpy()
+    result = np.round(output).astype(np.int)
 
     return result
--- Project/train/train.py
+++ Project/train/train.py
@@ -67,10 +67,30 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     device       - Where the model and data should be loaded (gpu or cpu).
     """
     
-    # TODO: Paste the train() method developed in the notebook here.
-
-    pass
-
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            
+            #reset the optimizer
+            optimizer.zero_grad()
+            #get model output
+            prediction = model(batch_X)
+            #get loss
+            loss = loss_fn(prediction, batch_y)
+            #backpropagate
+            loss.backward()
+            #update weights
+            optimizer.step()
+            
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 if __name__ == '__main__':
     # All of the model parameters and training parameters are sent as arguments when the script
